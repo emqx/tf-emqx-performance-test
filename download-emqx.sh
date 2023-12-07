8828c23484a0a4e5d70e68ed01074eb371ec9fa8
Ivan Dyachkov <dev@dyachkov.org>
2023-12-07 08:08:58 +0100

    chore: remove obsolete scripts

--- download-emqx.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-version=${1:-5.1.4}
-out=${2:-emqx-v${version}.deb}
-os=ubuntu20.04
-arch=amd64
-wget -nc https://github.com/emqx/emqx/releases/download/v${version}/emqx-${version}-${os}-${arch}.deb -O ${out}
