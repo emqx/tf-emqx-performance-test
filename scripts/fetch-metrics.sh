c705b8db8484ba699d0d0307ae6d7525e739e9b4
Ivan Dyachkov <dev@dyachkov.org>
2024-07-12 17:10:46 +0200

    feat: add ds/fan-in scenario

--- scripts/fetch-metrics.sh
+++ scripts/fetch-metrics.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 #set -x
 
 PROMETHEUS_URL=${PROMETHEUS_URL:-$(terraform output -raw prometheus_url)}
-PERIOD=${PERIOD:-30m}
+PERIOD=${PERIOD:-5m}
 
 # cpu
 curl -gs "$PROMETHEUS_URL/api/v1/query?query=(sum%20by(instance)%20(irate(node_cpu_seconds_total{mode!=\"idle\"}[$PERIOD]))/on(instance)%20group_left%20sum%20by%20(instance)((irate(node_cpu_seconds_total[$PERIOD]))))*100" | jq '.data.result[] | {"host": (.metric.instance|split(".")[0]), "cpu": (.value[1]|tonumber|.*100|round/100)}' | jq -rs > cpu.json
